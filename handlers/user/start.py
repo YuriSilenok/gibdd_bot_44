d9ea5974fe3f3c09d24a993c70e141b7d7bf219c
aleeeGG <vltrv50@gmail.com>
2025-05-15 16:58:48 +0300

    Отобразить кнопки при посылке команды /start для админа

--- handlers/user/start.py
+++ handlers/user/start.py
@@ -1,22 +1,27 @@
 """Обработка команды start"""
 from aiogram import Router
 from aiogram.filters import CommandStart
-from aiogram.types import Message
-from database.models import User
+from aiogram.types import Message, CallbackQuery
+from database.models import User, UserRole
+from keyboards.admin import ADMIN_START_KEYBOARD as ask
 
 router = Router()
 
 
 @router.message(CommandStart())
-async def cmd_start(message: Message):
+async def cmd_start(message: Message, callback: CallbackQuery):
     """Обработчик команды start"""
+
+    user = User.get_or_none(tg_id=message.from_user.id)
+
+    if not (user is None) and UserRole.get(tg_id=user).role == 'Администратор':
+        await callback.message.edit_reply_markup(reply_markup=ask)
+        return
 
     await message.answer(
         text='Добрый день.Через этого бота '
         'Вы можете отправить анонимное сообщение о пьяном водителе'
     )
-
-    user = User.get_or_none(tg_id=message.from_user.id)
 
     if user is None:
         User.create(
