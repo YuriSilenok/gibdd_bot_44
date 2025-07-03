fb75eaa114ea8d56e94d9c7c6ebdf504b9b2c756
YuriSilenok <yuri.silenok@gmail.com>
2025-07-03 15:05:35 +0300

    0.4.0

--- controller/employee.py
+++ controller/employee.py
@@ -1,9 +1,13 @@
 """Логика при взаимодействии над сотрудником"""
 
-from database.models import User, UserRole
+from database.models import Role, User, UserRole
 
 
 def is_employee(user: User) -> bool:
     """Проверка является ли пользователь сотрудником"""
 
-    return UserRole.get_or_none(user=user) is not None
+    return UserRole.select().where(UserRole.role.in_([
+        Role.get(name="Начальник"),
+        Role.get(name="Администратор"),
+        Role.get(name="Инспектор"),
+    ])).first() is not None
